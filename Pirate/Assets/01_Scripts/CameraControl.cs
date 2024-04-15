21e994d65486ffb5e9ba9bed69b1a5ce2ee147d8
minchae123 <08minchae17@gmail.com>
2024-04-15 10:33:41 +0900

    카메라라

--- Pirate/Assets/01_Scripts/CameraControl.cs
+++ Pirate/Assets/01_Scripts/CameraControl.cs
@@ -2,54 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UIElements;
 
 public class CameraControl : MonoBehaviour
 {
-	private Camera cam;
-
-	[SerializeField] private float rotateSpeed = 500f;
-	[SerializeField] private float zoomScale = 10f;
-	[SerializeField] private float minZoom = 0.1f;
-	[SerializeField] private float maxZoom = 10f;
-
-	private Vector3 mouseWorldPosStart;
-
-	private void Awake()
-	{
-		cam = Camera.main;
-	}
+	[SerializeField] private float speed;
 
 	private void Update()
 	{
-		if (Input.GetMouseButton(1))
+		float x = Input.GetAxis("Horizontal");
+		if(x != 0)
 		{
-			CameraOrbit();
+			transform.Rotate(new Vector3(30, -x * speed, 0));
+			transform.rotation = Quaternion.Euler(30, transform.rotation.eulerAngles.y, 0);
 		}
-
-		CameraZoom(Input.GetAxis("Mouse ScrollWheel"));
-	}
-
-	public void CameraOrbit()
-	{
-		if (Input.GetAxis("Mouse Y") != 0 || Input.GetAxis("Mouse X") != 0)
-		{
-			float vertical = Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
-			float horizontal = Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
-			transform.Rotate(Vector3.left, vertical);
-			transform.Rotate(Vector3.down, horizontal, Space.World);
-		}
-	}
-
-	public void CameraZoom(float scale)
-	{
-		if (scale != 0)
-		{
-			mouseWorldPosStart = cam.ScreenToWorldPoint(Input.mousePosition);
-			cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - scale * zoomScale, minZoom, maxZoom);
-			Vector3 mouseDiff = mouseWorldPosStart - cam.ScreenToWorldPoint(Input.mousePosition);
-			transform.position += mouseDiff;
-		}
-
 	}
 }
